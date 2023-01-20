ba5107efe02a4ad467e7566ce06eb715e6c91f3c
Алексей Хан <aleksddk@MacBook-Pro-Aleksej.local>
2023-01-20 19:49:49 +0300

    fix: fix zoom in bounds

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -4,18 +4,13 @@ import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 function Map(props:any) {
 
-  let {center, zoom, bounds, maxZoom} = props
+  let {center, zoom, bounds} = props
 
   function SetView() {
-    // const [bbounds, setBounds] = useState(bounds)
+
     const map = useMap()
 
-    useEffect(()=>{
-      if (bounds) {
-        // setBounds(bbounds)
-        map.fitBounds(bounds, {maxZoom: 12})
-      }
-    })
+    bounds && map.fitBounds(bounds, {padding: [50, 50]})
   
     return (
       <></>   
@@ -28,21 +23,24 @@ function Map(props:any) {
   return (
    <>
     {bounds ? 
-      (<MapContainer className={styles.mapContainer} bounds={bounds} maxZoom={maxZoom} scrollWheelZoom={false} >
+      (<MapContainer className={styles.mapContainer} bounds={bounds} scrollWheelZoom={false} >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[59.84660399, 30.29496392]}>
+
+        <Marker position={bounds[0]}>
           <Popup>
           Чтобы увидеть маршрут, выберите его в таблице
           </Popup>
         </Marker>
-        <Marker position={[59.82934196, 30.42423701]}>
+
+        <Marker position={bounds[1]}>
           <Popup>
           Чтобы увидеть маршрут, выберите его в таблице
         </Popup>
         </Marker>
+
         <SetView />
       </MapContainer>
       ) : 
--- src/components/mapCont/MapCont.tsx
+++ src/components/mapCont/MapCont.tsx
@@ -11,23 +11,16 @@ export default function MapCont() {
   const [center, setCenter] = useState(initialCenter)
 
     // let center: Array<number> = [55.757233, 37.618943]
-
     // let bounds: LatLngBoundsExpression = []
 
     const clickHandler = () => {
-        setBounds([ [59.84660399, 30.29496392],
-          [59.82934196, 30.42423701],])
+        setBounds([ [59.84660399, 30.29496392], [59.82934196, 30.42423701] ])
     }
-
-    // butStyle = {
-    //   height: '10px',
-    //   width: '40px'
-    // }
       
   return (
     <div>
-        <button type='button' onClick={clickHandler} >Button</button>
-       { bounds.length === 0 ? <Map center = {center} zoom={13}/> : <Map bounds={bounds} maxZoom={12}/>}
+        <button type='button' onClick={clickHandler}>Button</button>
+       { bounds.length === 0 ? <Map center = {center} zoom={13}/> : <Map bounds={bounds} />}
     </div>
   )
 }
